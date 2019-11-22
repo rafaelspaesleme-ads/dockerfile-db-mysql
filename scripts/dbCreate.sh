14b6d1e1acdf207040f66b35debc1480ef26a634
Rafael Paes Leme <11760799+rafaelspaesleme-ads@users.noreply.github.com>
2019-11-22 19:51:18 -0300

    updating database creation script.

--- scripts/dbCreate.sh
+++ scripts/dbCreate.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+mysql -u ${USER_X} -p
+
+GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'${ADDRESS_DB}' IDENTIFIED BY '${DB_PASS}';
+
+CREATE DATABASE ${DB_NAME};
+
+USE ${DB_NAME};
+
+# A PENSAR
+##CREATE TABLE ${NAME_TB} ( id smallint unsigned not null auto_increment, name varchar(20) not null, constraint pk_example primary key (id) ); INSERT INTO example ( id, name ) VALUES ( null, 'Sample data' );
